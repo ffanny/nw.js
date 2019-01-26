0a80f10ca12ed8e7a941717b1c1bc1c909900407
ffanny <1402826485@qq.com>
2019-01-26 16:24:06 +0800

    Update test.py

--- code/issue6784-webviewcrash-url-load/test.py
+++ code/issue6784-webviewcrash-url-load/test.py
@@ -1,13 +1,8 @@
 import time
 import os
-import urlparse, urllib
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from nw_util import *
-
-def path2url(path):
-        return urlparse.urljoin(
-                  'file:', urllib.pathname2url(path))
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
